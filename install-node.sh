26bf105ad1f4e6fc1537656ee9f36dae6a7e4262
Kevin Lewis <kevin.k.lewis@gmail.com>
2015-11-18 13:03:24 -0600

    Added support for node 5.1.0

--- install-node.sh
+++ install-node.sh
@@ -1,4 +1,5 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 export NVM_DIR=$HOME/.nvm
 . ~/.nvm/nvm.sh
 nvm install 0.12.7
+nvm install 5.1.0
