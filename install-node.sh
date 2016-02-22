81e9de5828402324cd564a8022f5910a5305cd15
Kevin Lewis <kevin.k.lewis@gmail.com>
2016-02-22 10:19:37 -0600

    Ugraded NodeJS and NVM

--- install-node.sh
+++ install-node.sh
@@ -1,4 +1,4 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 export NVM_DIR=$HOME/.nvm
 . ~/.nvm/nvm.sh
-nvm install 5.4.1
+nvm install 5.6.0
